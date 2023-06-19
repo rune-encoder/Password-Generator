8cef5a8b1cf9796263b98028f6abee4a0d63961a
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-06-18 23:55:42 -0500

    Refactored If Statements and Added Detailed Comments

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Object: Contains organized list of letters, numbers, and special characters used in our Password Generator.
+// Variable Characters: Contains Key-Values for Numbers, Upper and Lower case letters and Special Characters.
 var characters = {
   numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
   upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
@@ -5,75 +7,108 @@ var characters = {
   special: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
 }
 
+// Variable for Letters: Merges Upper and Lower case letters into one Array.
 var lettersChar = characters.upperCase.concat(characters.lowerCase);
 
+// This function is used to determine User's password choices: 
+// Choices: 1. Number of characters in password, 2. Requests: Numbers, 3. Requests Special Characters. 
 function determineUserPass() {
   var userPassOptions = {
+    // Here user will: 
+    // 1. Input requested number of characters (Returns a number), 
     charLength: parseInt(prompt(("How many characters do you want? "))),
+    // 2. Input if they requests number characters (Returns true or false),
     hasNumberChar: confirm("Click OK if you'd like numeric characters generated. "),
+    // 3. Input if they requests special characters (Returns true or false).
     hasSpecialChar: confirm("Click OK if you'd like special characters generated. ")
   };
   return userPassOptions
 }
 
+// This Function creates a random number based on the specified arrays length.
+// The number represents the index in an specified array chosen randomly
 function randomize(array) {
   var randomIndex = Math.floor(Math.random() * array.length);
+  // Note: Brackets are used to indicate a index from an array. 
+  // The random number generated will be used obtain a random index from our character array.
     return array[randomIndex]
 }
 
+// This function creates the user's desired password.
 function createPassword () {
+  // Options: will produce an object with the user's chosen properties. 
+  // The function used to determine the user's options is called here and declared as a variable.
   var options = determineUserPass();
+  // Chosen Characters: Will contain a list of all the possible and requested characters used for the password.
   var chosenCharacters = [];
+  // Guaranteed Characters: Will contain a number, special character or both that will added to the password to gaurentee user's chosen options.
   var guaranteedChar = [];
+  // Result Characters: Will contain the *FINAL PRODUCT*. The desired password.
   var result = [];
 
-  if (options.hasNumberChar === true && options.hasSpecialChar === true) {
+  // If user desires Numbers and Special Characters: 
+  if (options.hasNumberChar && options.hasSpecialChar) {
+    // 1. Join all Letters, Numbers, and Special Characters into one array. 
     chosenCharacters = lettersChar.concat(characters.numbers, characters.special);
+    // 2. Create one random Number one Special Character: Will be added to password no matter what.
     guaranteedChar.push(randomize(characters.numbers));
     guaranteedChar.push(randomize(characters.special));
-  } else if (options.hasNumberChar === true && options.hasSpecialChar === false) { 
+
+  // If user desires numbers: 
+  } else if (options.hasNumberChar && !options.hasSpecialChar) { 
+    // 1. Join all Letters and Numbers into one array. 
     chosenCharacters = lettersChar.concat(characters.numbers);
+    // 2. Create one random Number: Will be added to password no matter what.
     guaranteedChar.push(randomize(characters.numbers));
-  } else if (options.hasNumberChar === false && options.hasSpecialChar === true) {
+
+  // If user desires special characters: 
+  } else if (!options.hasNumberChar && options.hasSpecialChar) {
+    // 1. Join all Letters and Special Characters into one array. 
     chosenCharacters = lettersChar.concat(characters.special);
+    // 2. Create one random Special Character: Will be added to password no matter what.
     guaranteedChar.push(randomize(characters.special));
+
+  // If user DOES NOT desire Numbers or Special Characters.  
   } else {
+    // 1. Only Upper and Lower case Letters will be added into possible characters. 
     chosenCharacters = lettersChar;
   }
   
+  // Random characters are generated and chosen here.
   for (var i = 0; i < options.charLength; i++) {
+    // Calls function to generate random index number used to pull a character from our chosen characters array.
     randomChar = randomize(chosenCharacters);
+    // Pushes random generated character into our results list.
+    // Process is repeated based on user's chosen password length.
     result.push(randomChar);
-    console.log(result);
-    console.log("");
   }
 
+  // Will choose our guaranteed character(s) created in our if statement that will be exchanged starting from the first index in our results.
   for (var i = 0; i < guaranteedChar.length; i++) {
     result[i] = guaranteedChar[i];
-    console.log(result);
-    console.log("");
   }
 
-  console.log(guaranteedChar);
-
+  // Using result.join will combine our all our individually created password strings into one whole string.
+  // Results in our password. *Final Product*
   return result.join("")
 }
 
-// // Assignment Code
+// Assignment Code
+// Will target our "Generate" button using the id="generate".
 var generateBtn = document.querySelector("#generate");
 
-// // Write password to the #password input
+// Write password to the #password input
 function writePassword() {
+  // This calls our function which creates the password.
   var password = createPassword();
-
+  // The textarea in the document is targeted with the id="password".
   var passwordText = document.querySelector("#password");
-
+  // The password created is inputed into the text area inside id="password".
   passwordText.value = password;
 }
 
-writePassword();
-
-// // Add event listener to generate button
+// Add event listener to generate button. 
+// Once we click the button with the id="generate" our code will be ran.
 generateBtn.addEventListener("click", writePassword);
 
-
+//!!!To run our functions and code CLICK THE GENERATE BUTTON!!!
